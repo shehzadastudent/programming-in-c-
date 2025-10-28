7d866107b36134ddc8a3c8a05f10179164a0b766
shehzadastudent <shehzadastudent@gmail.com>
2025-10-28 22:19:57 +0530

    Add files via upload

---
+++ siandci.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    printf("Name - Syed Multazam Ahmed Chishty\nSAP ID - 590028251\nCourse - BCA\nBatch - B5\n");
+    printf("---------------------------------------------\n");
+
+    float principal, rate, time;
+    float simpleInterest, compoundInterest;
+
+    printf("Enter Principal, Rate and Time: ");
+    scanf("%f %f %f", &principal, &rate, &time);
+
+    // Simple Interest = (P × R × T) /
+
