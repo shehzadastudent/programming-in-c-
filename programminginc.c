73f076cbade91b6ac9d5d14742bd605eb8d8d67b
shehzadastudent <shehzadastudent@gmail.com>
2025-10-28 22:07:26 +0530

    Add files via upload
    100 days of code day 1 q1

---
+++ programminginc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n,i, sum=0;
+    printf("Name - Syed Multazam Ahmed Chishty\nSAP ID - 590028251\nCourse - BCA\nBatch - B5\n");
+    printf("---------------------------------------------\n");
+
+    printf("Enter a number ");
+    scanf("%d", &n);
+ printf("Enter a number ");
+    scanf("%d", &i);
+    sum =n+i;
+    printf("sum of both numbers is : %d",sum);
+    
+    
+}
